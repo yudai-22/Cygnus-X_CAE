1ea702b73011085f8174e4ef1ec67106fa05d514
fujimoto <progaress21@gmail.com>
2026-03-06 22:31:45 +0900

    chage color map => viridis

--- Binary_classification/data/FUGIN/fgn_make_no_bubble.py
+++ Binary_classification/data/FUGIN/fgn_make_no_bubble.py
@@ -267,7 +267,7 @@ for fits_num, (fits_path, integ_fits_path) in enumerate(zip(fits_path_all, integ
             # 積分処理
             integ_data = np.nansum(data, axis=0)
 
-            axes_int_flat[i].imshow(integ_data, cmap="cividis", ) # お好みでcmapを指定
+            axes_int_flat[i].imshow(integ_data, cmap="viridis", ) # お好みでcmapを指定
             axes_int_flat[i].axis("off")
             axes_int_flat[i].set_title(f"cut_number {num}")
 
